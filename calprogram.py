798f65c8f5e53cbc1b49cd504fe33acfc10b7acf
Collins Munyendo <collinsmunyendo@Collinss-MacBook-Pro.local>
2020-01-27 23:03:39 +0300

    Added delete as well as clear all functionality

--- calprogram.py
+++ calprogram.py
@@ -11,6 +11,20 @@ def get_variable(num):
     display.insert(i, num)
     num += 1
 
+def clear_all():
+    display.delete(0, END)
+
+def back_space():
+    entire_string = display.get()
+    if len(entire_string):
+        new_string = entire_string[:-1]
+        clear_all()
+        display.insert(0, new_string)
+
+    else:
+        clear_all()
+        display.insert(0, "Error")
+
 # adding the input fields
 display = Entry(root)
 display.grid(row=1, columnspan=6, sticky=W+E)
@@ -29,7 +43,7 @@ Button(root, text="8", command=lambda : get_variable(8)).grid(row=4, column=1)
 Button(root, text="9", command=lambda : get_variable(9)).grid(row=4, column=2)
 
 # adding other buttons to the calculator
-Button(root, text="AC").grid(row=5, column=0)
+Button(root, text="AC", command=lambda : clear_all()).grid(row=5, column=0)
 Button(root, text="0", command=lambda : get_variable(0)).grid(row=5, column=1)
 Button(root, text="=").grid(row=5, column=2)
 
@@ -44,7 +58,7 @@ Button(root, text="%").grid(row=3, column=4)
 Button(root, text="(").grid(row=4, column=4)
 Button(root, text="exp").grid(row=5, column=4)
 
-Button(root, text="<-").grid(row=2, column=5)
+Button(root, text="<-", command=lambda : back_space()).grid(row=2, column=5)
 Button(root, text="x!").grid(row=3, column=5)
 Button(root, text=")").grid(row=4, column=5)
 Button(root, text="^2").grid(row=5, column=5)
