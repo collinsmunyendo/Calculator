5ffa64b322b99ecbc30a956d535d7d4edb93e2ca
Collins Munyendo <collinsmunyendo@Collinss-MacBook-Pro.local>
2020-01-28 22:57:33 +0300

    Added functionality for the operators including addition, multiplicationa so forth

--- calprogram.py
+++ calprogram.py
@@ -9,7 +9,26 @@ i = 0
 def get_variable(num):
     global i
     display.insert(i, num)
-    num += 1
+    i += 1
+
+# other functions
+def calculate():
+    entire_string = display.get()
+    try:
+        a = parser.expr(entire_string).compile()
+        result = eval(a)
+        clear_all()
+        display.insert(0, result)
+
+    except Exception:
+        clear_all()
+        display.insert(0, "Error")
+
+def get_operator(operator):
+    global  i
+    length = len(operator)
+    display.insert(i, operator)
+    i += length
 
 def clear_all():
     display.delete(0, END)
@@ -45,22 +64,22 @@ Button(root, text="9", command=lambda : get_variable(9)).grid(row=4, column=2)
 # adding other buttons to the calculator
 Button(root, text="AC", command=lambda : clear_all()).grid(row=5, column=0)
 Button(root, text="0", command=lambda : get_variable(0)).grid(row=5, column=1)
-Button(root, text="=").grid(row=5, column=2)
+Button(root, text="=", command=lambda : calculate()).grid(row=5, column=2)
 
-Button(root, text="+").grid(row=2, column=3)
-Button(root, text="-").grid(row=3, column=3)
-Button(root, text="*").grid(row=4, column=3)
-Button(root, text="/").grid(row=5, column=3)
+Button(root, text="+", command=lambda : get_operator("+")).grid(row=2, column=3)
+Button(root, text="-", command=lambda : get_operator("-")).grid(row=3, column=3)
+Button(root, text="*", command=lambda : get_operator("*")).grid(row=4, column=3)
+Button(root, text="/", command=lambda : get_operator("/")).grid(row=5, column=3)
 
 # adding new operations
-Button(root, text="pi").grid(row=2, column=4)
-Button(root, text="%").grid(row=3, column=4)
-Button(root, text="(").grid(row=4, column=4)
-Button(root, text="exp").grid(row=5, column=4)
+Button(root, text="pi", command=lambda : get_operator("*3.14")).grid(row=2, column=4)
+Button(root, text="%", command=lambda : get_operator("%")).grid(row=3, column=4)
+Button(root, text="(", command=lambda : get_operator("(")).grid(row=4, column=4)
+Button(root, text="exp", command=lambda : get_operator("**")).grid(row=5, column=4)
 
 Button(root, text="<-", command=lambda : back_space()).grid(row=2, column=5)
 Button(root, text="x!").grid(row=3, column=5)
-Button(root, text=")").grid(row=4, column=5)
-Button(root, text="^2").grid(row=5, column=5)
+Button(root, text=")", command=lambda : get_operator(")")).grid(row=4, column=5)
+Button(root, text="^2", command=lambda : get_operator("**2")).grid(row=5, column=5)
 
 root.mainloop()
